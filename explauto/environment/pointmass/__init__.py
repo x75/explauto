23193f1780f99d6694040e49e7b12f1e0324db5c
Oswald Berthold <bertolos@informatik.hu-berlin.de>
2016-06-30 12:39:02 +0200

    updating pointmass environment

--- explauto/environment/pointmass/__init__.py
+++ explauto/environment/pointmass/__init__.py
@@ -1,23 +1,80 @@
 from numpy import array, pi, sqrt, cos, sin, linspace, zeros
 from numpy import random
+import numpy as np
 
 
 from ...exceptions import ExplautoNoTestCasesError
 from .pointmass import PointmassEnvironment
 
 
-def make_pointmass_config(m_ndims, m_max, s_mins, s_maxs, mass, sysnoise, sensnoise):
-    return dict(m_mins=array([-m_max] * m_ndims),
+def make_pointmass_config(st_ndims, m_ndims, s_ndims, sensor_transform, m_max, s_mins, s_maxs, mass, sysnoise, sensnoise):
+    return dict(st_ndims = st_ndims,
+                m_ndims = m_ndims,
+                s_ndims = s_ndims,
+                sensor_transform = sensor_transform,
+                m_mins=array([-m_max] * m_ndims),
                 m_maxs=array([m_max] * m_ndims),
                 s_mins=s_mins,
                 s_maxs=s_maxs,
                 mass=mass,
                 sysnoise = sysnoise,
-                sensnoise = sensnoise)
+                sensnoise = sensnoise
+    )
 
-low_dim = make_pointmass_config(1, 1, array([-10.0, -10.0]),      array([10., 10.]),      1, 0.02, 0.02)
-mid_dim = make_pointmass_config(3, 1, array([-10.0, -10.0] *  3), array([10., 10.] *  3), 1, 0.02, 0.02)
-hd_dim = make_pointmass_config(10, 1, array([-10.0, -10.0] * 10), array([10., 10.] * 10), 1, 0.02, 0.02)
+low_dim_vel = make_pointmass_config(st_ndims = 3,
+                                m_ndims = 1,
+                                s_ndims = 1,
+                                sensor_transform = np.array([[0, 1, 0]]),
+                                m_max = 1,
+                                s_mins = array([-10.0]),
+                                s_maxs = array([ 10.0]),
+                                mass = 1,
+                                sysnoise = 0.02,
+                                sensnoise = 0.02)
+
+low_dim_full = make_pointmass_config(st_ndims = 3,
+                                m_ndims = 1,
+                                s_ndims = 3,
+                                sensor_transform = np.eye(3),
+                                m_max = 1,
+                                s_mins = array([-10.0, -10.0, -10.0] * 3),
+                                s_maxs = array([ 10.0,  10.0,  10.0] * 3),
+                                mass = 1,
+                                sysnoise = 0.02,
+                                sensnoise = 0.02)
+
+mid_dim_vel = make_pointmass_config(st_ndims = 9,
+                                m_ndims = 3,
+                                s_ndims = 3,
+                                sensor_transform = np.array([
+                                    [0, 0, 0, 1, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 1, 0, 0, 0]]),
+                                m_max = 1,
+                                s_mins = array([-10.0] *  3),
+                                s_maxs = array([ 10.0] *  3),
+                                mass = 1,
+                                sysnoise = 0.02,
+                                sensnoise = 0.02)
+
+mid_dim_full = make_pointmass_config(st_ndims = 9,
+                                m_ndims = 3,
+                                s_ndims = 9,
+                                sensor_transform = np.eye(9),
+                                m_max = 1,
+                                s_mins = array([-10.0, -10.0, -10.0] *  3),
+                                s_maxs = array([ 10.0,  10.0,  10.0] *  3),
+                                mass = 1,
+                                sysnoise = 0.02,
+                                sensnoise = 0.02)
+
+# hd_dim = make_pointmass_config(st_ndims = 30,
+#                                m_ndims = 10,
+#                                s_ndims = 10,
+#                                sensor_transform = np.array([
+#                                    [0] * 10 + [1] + [0] * 19,
+#                                ]),
+#                                 1, array([-10.0, -10.0] * 10), array([10., 10.] * 10), 1, 0.02, 0.02)
 # hd_dim = make_arm_config(30, pi/8., array([-0.6, -0.9]), array([1., 0.9]), 1., 0.001)
 
 # hd_dim_range = make_arm_config(30, pi/8., array([-2., -2.]), array([2., 2.]), 1., 0.001)
@@ -25,11 +82,15 @@ hd_dim = make_pointmass_config(10, 1, array([-10.0, -10.0] * 10), array([10., 10
 # hd_dim_range = make_arm_config(50, pi/12., array([-2., -2.]), array([2., 2.]), 1., 0.02)
 
 environment = PointmassEnvironment
-configurations = {'low_dimensional': low_dim,
-                  'mid_dimensional': mid_dim,
-                  'high_dimensional': hd_dim,
-#                  'high_dim_high_s_range': hd_dim_range,
-                  'default': low_dim}
+configurations = {
+    'low_dim_full': low_dim_full,
+    'mid_dim_full': mid_dim_full,
+    'low_dim_vel': low_dim_vel,
+    'mid_dim_vel': mid_dim_vel,
+    # 'high_dimensional': hd_dim,
+    # 'high_dim_high_s_range': hd_dim_range,
+    'default': low_dim_vel
+}
 
 
 def testcases(config_str, n_samples=100):
--- explauto/environment/pointmass/pointmass.py
+++ explauto/environment/pointmass/pointmass.py
@@ -8,15 +8,24 @@ import pointmasslib
 from ..environment import Environment
 
 class PointmassEnvironment(Environment):
-    def __init__(self, m_mins, m_maxs, s_mins, s_maxs, sysnoise = 0.0, sensnoise = 0.0, force_max = 1, mass = 1.0, dt = 0.1):
+    def __init__(self, st_ndims, m_ndims, s_ndims, sensor_transform, m_mins, m_maxs, s_mins, s_maxs, sysnoise = 0.0, sensnoise = 0.0, force_max = 1, mass = 1.0, dt = 0.1):
         Environment.__init__(self, m_mins, m_maxs, s_mins, s_maxs)
 
+        # FIXME: dimensions
+        # - world/state dimension
+        self.state_dim = st_ndims # pos, vel, acc
+        self.world_dim = st_ndims / 3
+        # - motor dimension
+        self.motor_dim = m_ndims
+        # - sensor/observable dimension
+        self.sensor_dim = s_ndims
+        self.sensor_transform = sensor_transform # state to sensor transformation matrix
+                
         print "self.conf", self.conf
         self.sysnoise  = sysnoise
         self.sensnoise = sensnoise
-        # FIXME: distinguish between intrinsic state dimension and sensor / motor dimensions
-        self.spacedims = self.conf.s_ndims/2
-        self.x0 = np.zeros((self.conf.s_ndims, 1)) # [0, 0] * self.conf.ndims
+        # self.spacedims = self.conf.s_ndims/2
+        self.x0 = np.zeros((self.state_dim, 1)) # [0, 0] * self.conf.ndims
         self.dt = dt
         self.force_max = force_max
         self.mass = mass
@@ -31,19 +40,24 @@ class PointmassEnvironment(Environment):
     def apply_force(self, u):
         # print "u", u
         # FIXME: insert motor transfer function
-        a = (u/self.mass).reshape((self.spacedims, 1))
+        a = (u/self.mass).reshape((self.world_dim, 1))
+        # print("a.shape", a.shape)
         # print "a", a, self.x[self.conf.s_ndims/2:]
-        v = self.x[self.conf.s_ndims/2:] + a * self.dt
+        v = self.x[self.world_dim:self.world_dim*2] + a * self.dt
         # print "v", v
-        p = self.x[:self.conf.s_ndims/2] + v * self.dt
+        p = self.x[:self.world_dim] + v * self.dt
 
-        self.x[:self.conf.s_ndims/2] = p.copy()
-        self.x[self.conf.s_ndims/2:] = v.copy()
+        self.x[:self.world_dim] = p.copy()
+        self.x[self.world_dim:self.world_dim*2] = v.copy()
+        self.x[self.world_dim*2:] = a.copy()
 
         self.x += self.sysnoise * random.randn(self.x.shape[0], self.x.shape[1])
         
         # return x
         # self.x = x # pointmasslib.simulate(self.x, [u], self.dt)
+
+    def step(self, m):
+        return self.compute_sensori_effect(m)
 
     def compute_sensori_effect(self, m):
         # for u in self
@@ -51,9 +65,10 @@ class PointmassEnvironment(Environment):
         # res = array(self.x)
         # print res
         # print self.noise
-        # print self.x.shape
-        self.x += self.sensnoise * random.randn(self.x.shape[0], self.x.shape[1])
-        return self.x
+        # print "self.x.shape", self.x.shape
+        sensors = np.dot(self.sensor_transform, self.x)
+        sensors += self.sensnoise * random.normal(0, 1.0, (self.sensor_dim, 1))
+        return sensors
 
     def plot_current_state(self, ax):
         # ax.plot(
