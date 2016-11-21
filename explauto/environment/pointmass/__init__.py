5443d5afb1c433bea713ad2c6b6604ed3adc0b2c
Oswald Berthold <bertolos@informatik.hu-berlin.de>
2016-11-21 11:09:14 +0100

    pointmass amendments

--- explauto/environment/pointmass/__init__.py
+++ explauto/environment/pointmass/__init__.py
@@ -7,7 +7,7 @@ from ...exceptions import ExplautoNoTestCasesError
 from .pointmass import PointmassEnvironment
 
 
-def make_pointmass_config(st_ndims, m_ndims, s_ndims, sensor_transform, m_max, s_mins, s_maxs, mass, sysnoise, sensnoise, doRos = False):
+def make_pointmass_config(st_ndims, m_ndims, s_ndims, sensor_transform, m_max, s_mins, s_maxs, mass, sysnoise, sensnoise, doRos = False, control_order = 2):
     return dict(st_ndims = st_ndims,
                 m_ndims = m_ndims,
                 s_ndims = s_ndims,
@@ -19,24 +19,14 @@ def make_pointmass_config(st_ndims, m_ndims, s_ndims, sensor_transform, m_max, s
                 mass=mass,
                 sysnoise = sysnoise,
                 sensnoise = sensnoise,
-                doRos = doRos
+                doRos = doRos,
+                control_order = control_order
     )
 
 low_dim_vel = make_pointmass_config(st_ndims = 3,
                                 m_ndims = 1,
                                 s_ndims = 1,
                                 sensor_transform = np.array([[0, 1, 0]]),
-                                m_max = 1.0,
-                                s_mins = array([-1.0]),
-                                s_maxs = array([ 1.0]),
-                                mass = 1,
-                                sysnoise = 0.00,
-                                sensnoise = 0.00)
-
-low_dim_acc_vel = make_pointmass_config(st_ndims = 3,
-                                m_ndims = 1,
-                                s_ndims = 1,
-                                sensor_transform = np.array([[0, 1, 0], [0, 0, 1]]),
                                 m_max = 1.0,
                                 s_mins = array([-1.0]),
                                 s_maxs = array([ 1.0]),
@@ -55,6 +45,7 @@ low_dim_full = make_pointmass_config(st_ndims = 3,
                                 sysnoise = 0.02,
                                 sensnoise = 0.02)
 
+# mid dimensional
 mid_dim_vel = make_pointmass_config(st_ndims = 9,
                                 m_ndims = 3,
                                 s_ndims = 3,
@@ -103,6 +94,108 @@ high_dim_vel = make_pointmass_config(st_ndims = 3 * 10,
                                 sysnoise = 0.0,
                                 sensnoise = 0.0)
 
+# smq, implies _c2 control 2nd order (force) with respect to position
+low_dim_acc_vel = make_pointmass_config(st_ndims = 3,
+                                m_ndims = 1,
+                                s_ndims = 2,
+                                sensor_transform = np.array([
+                                    [0, 1, 0],
+                                    [0, 0, 1]
+                                    ]),
+                                m_max = 1.0,
+                                s_mins = array([-1.0]),
+                                s_maxs = array([ 1.0]),
+                                mass = 1,
+                                sysnoise = 0.00,
+                                sensnoise = 0.00)
+
+# smq, this is _c1, control 1st order (velocity) with respect to position
+low_dim_acc_vel_c1 = make_pointmass_config(st_ndims = 3,
+                                m_ndims = 1,
+                                s_ndims = 2,
+                                sensor_transform = np.array([
+                                    [0, 1, 0],
+                                    [0, 0, 1]
+                                    ]),
+                                m_max = 1.0,
+                                s_mins = array([-1.0]),
+                                s_maxs = array([ 1.0]),
+                                mass = 1,
+                                sysnoise = 0.00,
+                                sensnoise = 0.00,
+                                control_order = 1)
+
+# smq
+planar_dim_acc_vel = make_pointmass_config(st_ndims = 6,
+                                m_ndims = 2,
+                                s_ndims = 4,
+                                sensor_transform = np.array([
+                                    [0, 0, 1, 0, 0, 0],
+                                    [0, 0, 0, 1, 0, 0],
+                                    [0, 0, 0, 0, 1, 0],
+                                    [0, 0, 0, 0, 0, 1],
+                                ]),
+                                m_max = 1.0,
+                                s_mins = array([-1.0, -1.0]),
+                                s_maxs = array([ 1.0,  1.0]),
+                                mass = 1,
+                                sysnoise = 0.00,
+                                sensnoise = 0.00)
+# smq
+mid_dim_acc_vel = make_pointmass_config(st_ndims = 9,
+                                m_ndims = 3,
+                                s_ndims = 6,
+                                sensor_transform = np.array([
+                                    # [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    # [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    # [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 1, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 1, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 1, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 1, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 1],
+                                    ]),
+                                m_max = 1,
+                                s_mins = array([-1.0] *  3),
+                                s_maxs = array([ 1.0] *  3),
+                                mass = 1.0,
+                                sysnoise = 0.005,
+                                sensnoise = 0.005,
+                                doRos = True)
+# smq
+high_dim_acc_vel = make_pointmass_config(st_ndims = 3 * 10,
+                                m_ndims = 10,
+                                s_ndims = 2 * 10,
+                                sensor_transform = np.array([
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                                    ]),
+                                m_max = 1,
+                                s_mins = array([-1.0] *  10),
+                                s_maxs = array([ 1.0] *  10),
+                                mass = 1.0,
+                                sysnoise = 0.0,
+                                sensnoise = 0.0)
+
 # hd_dim = make_pointmass_config(st_ndims = 30,
 #                                m_ndims = 10,
 #                                s_ndims = 10,
@@ -123,7 +216,13 @@ configurations = {
     'low_dim_vel':  low_dim_vel,
     'mid_dim_vel':  mid_dim_vel,
     'high_dim_vel': high_dim_vel,
-    'low_dim_acc_vel':  low_dim_acc_vel,
+    # smq
+    'low_dim_acc_vel':     low_dim_acc_vel,
+    'planar_dim_acc_vel':  planar_dim_acc_vel,
+    'mid_dim_acc_vel':     mid_dim_acc_vel,
+    'high_dim_acc_vel':     high_dim_acc_vel,
+    # smq kinematic
+    'low_dim_acc_vel_c1':     low_dim_acc_vel_c1,
     # 'high_dimensional': hd_dim,
     # 'high_dim_high_s_range': hd_dim_range,
     'default': low_dim_vel
--- explauto/environment/pointmass/pointmass.py
+++ explauto/environment/pointmass/pointmass.py
@@ -10,7 +10,7 @@ from ..environment import Environment
 from ...utils import bounds_min_max
 
 class PointmassEnvironment(Environment):
-    def __init__(self, st_ndims, m_ndims, s_ndims, sensor_transform, m_mins, m_maxs, s_mins, s_maxs, sysnoise = 0.0, sensnoise = 0.0, force_max = 1, mass = 1.0, dt = 0.1, doRos = False):
+    def __init__(self, st_ndims, m_ndims, s_ndims, sensor_transform, m_mins, m_maxs, s_mins, s_maxs, sysnoise = 0.0, sensnoise = 0.0, force_max = 1, mass = 1.0, dt = 0.1, doRos = False, control_order = 2, friction = 0.01):
         Environment.__init__(self, m_mins, m_maxs, s_mins, s_maxs)
 
         # FIXME: dimensions
@@ -22,6 +22,9 @@ class PointmassEnvironment(Environment):
         # - sensor/observable dimension
         self.s_ndims = s_ndims
         self.sensor_transform = sensor_transform # state to sensor transformation matrix
+
+        # check wether configured number of sensors matches sensor transform
+        assert(self.sensor_transform.shape[0] == self.s_ndims)
                 
         # print "%s.__init__: self.conf = %s" % (self.__class__.__name__, self.conf)
         self.sysnoise  = sysnoise
@@ -32,8 +35,10 @@ class PointmassEnvironment(Environment):
         self.dt = dt
         self.force_max = force_max
         self.mass = mass
-        self.friction = 0.01
+        self.friction = friction
         self.x = self.x0.copy()
+        # control mode: kinematic, dynamic
+        self.control_order = control_order
 
         self.sm_delays = dict()
         for i in range(self.s_ndims):
@@ -45,7 +50,9 @@ class PointmassEnvironment(Environment):
         self.cnt = 0
 
         self.doRos = doRos
-        
+
+        self.pubfuncs = []
+                
         if self.doRos:
             import rospy
             from std_msgs.msg import Float32MultiArray
@@ -54,15 +61,36 @@ class PointmassEnvironment(Environment):
             self.pubs["pos"] = rospy.Publisher("/robot/0/pos", Float32MultiArray)
             self.msgs = {}
             self.msgs["pos"] = Float32MultiArray()
+            self.pubfuncs.append(self.pub_ros)
         
     def compute_motor_command(self, ag_state):
-        print "ag_state", ag_state
+        # print "%s.compute_motor_command arg ag_state %s" % (self.__class__.__name__, ag_state)
         return bounds_min_max(ag_state, self.conf.m_mins, self.conf.m_maxs)
 
     def reset(self):
         self.x = self.x0.copy()
 
+    def pub_ros(self):
+        if self.doRos:
+            self.msgs["pos"].data = []
+            # print self.x.tolist()
+            # self.msgs["pos"].data = (self.x * 0.1).flatten().tolist()
+            self.msgs["pos"].data = self.x_.flatten().tolist()
+            self.pubs["pos"].publish(self.msgs["pos"])
+
+    def pub(self):
+        for p in self.pubfuncs:
+            p()
+
+    def scale(self):
+        # FIXME: what is that?
+        for i in range(self.world_dim):
+            self.x_[i] = self.x[i,0] * 0.1
+            # self.x_[1] = self.x[1,0] * 0.1
+            # self.x_[2] = self.x[2,0] * 0.1
+            
     def apply_force(self, u):
+        """control pointmass with force command (2nd order)"""
         # print "u", u
         # FIXME: insert motor transfer function
         a = (u/self.mass).reshape((self.world_dim, 1))
@@ -93,30 +121,42 @@ class PointmassEnvironment(Environment):
 
         self.x += self.sysnoise * random.randn(self.x.shape[0], self.x.shape[1])
 
-        for i in range(self.world_dim):
-            self.x_[i] = self.x[i,0] * 0.1
-            # self.x_[1] = self.x[1,0] * 0.1
-            # self.x_[2] = self.x[2,0] * 0.1
-
         # print "self.x[2,0]", self.x[2,0]
 
-        if self.doRos:
-            self.msgs["pos"].data = []
-            # print self.x.tolist()
-            # self.msgs["pos"].data = (self.x * 0.1).flatten().tolist()
-            self.msgs["pos"].data = self.x_.flatten().tolist()
-            self.pubs["pos"].publish(self.msgs["pos"])
-        
+        self.scale()
+        self.pub()                
         self.cnt += 1
         
         # return x
         # self.x = x # pointmasslib.simulate(self.x, [u], self.dt)
 
+    def apply_vel(self, u):
+        """control pointmass with velocity command (1st order)"""
+        v = u # self.x[self.world_dim:self.world_dim*2] * (1 - self.friction) + a * self.dt
+        p = self.x[:self.world_dim] + v * self.dt
+        a = (v - self.x[self.world_dim:self.world_dim*2]) / self.dt
+
+        self.x[:self.world_dim] = p.copy()
+        self.x[self.world_dim:self.world_dim*2] = v.copy()
+        self.x[self.world_dim*2:] = a.copy()
+
+        self.x += self.sysnoise * random.randn(self.x.shape[0], self.x.shape[1])
+        
+        self.scale()
+        self.pub()                
+        self.cnt += 1
+        
     def step(self, m):
         return self.compute_sensori_effect(m)
 
     def compute_sensori_effect(self, m):
-        self.apply_force(m)
+        if self.control_order == 2:
+            self.apply_force(m)
+        elif self.control_order == 1:
+            self.apply_vel(m)
+        elif self.control_order == 0:
+            self.apply_pos(m)
+            
         # create measurements as linear combinations from states via sensor_transform matrix
         sensors = np.dot(self.sensor_transform, self.x) # .flatten()
         sensors += self.sensnoise * random.normal(0, 1.0, sensors.shape) # (self.sensor_dim, 1)
